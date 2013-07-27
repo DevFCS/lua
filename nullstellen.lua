93ba131125a60b83fea251a1cceec1751845758a
trfunk <tristan.funk@ymail.com>
2013-07-27 16:32:44 +0200

    replaced tonumber(io.read()) with io.read(*n)

--- nullstellen.lua
+++ nullstellen.lua
@@ -3,11 +3,11 @@ print("Function:")								--asking for a function
 local fx = io.read()								--gets input
 print(fx, type(fx))								--print type of fx
 print("Start value: ")							--asking for a start value
-local startvalue = tonumber(io.read()) or -5		--gets input, converts it to a number or sets it to -5
+local startvalue = io.read(*n) or -5				--gets inpu or sets it to -5
 print("End value:")								--asking for endvalue
-local endvalue = tonumber(io.read()) or 5			--gets input, converts it to a number or sets it to 5
+local endvalue = io.read(*n) or 5					--gets input or sets it to 5
 print("Step:")									--asking for a step value
-local step = tonumber(io.read()) or 1				--gets input, converts it to a number or sets it to 1
+local step = io.read(*n) or 1						--gets input or sets it to 1
 for x = startvalue, endvalue, step do				-- numeric for loop
- print (fx , x)									-- printing fx and x	
+print (fx, x)									-- printing fx and x	
 end
