1548ac42a151b31f50d8d39e300228a4b6640909
trfunk <tristan.funk@ymail.com>
2013-07-27 16:24:33 +0200

    i guess it shouldn't be a string

--- nullstellen.lua
+++ nullstellen.lua
@@ -1,15 +1,13 @@
 print(" Checks if f(x) is Zero in the domain of definition")
-print("Function:")
-local fx = io.read()
-prntfunc = function (x) 
- return fx end
-print(fx, type(fx))
-print("Start value: ")
-local startvalue = tonumber(io.read()) or -5
-print("End value:")
-local endvalue = tonumber(io.read()) or 5
-print("Step:")
-local step = tonumber(io.read()) or 1
-for x = startvalue, endvalue, step do
- print (fx , x)
+print("Function:")								--asking for a function
+local fx = io.read()								--gets input
+print(fx, type(fx))								--print type of fx
+print("Start value: ")							--asking for a start value
+local startvalue = tonumber(io.read()) or -5		--gets input, converts it to a number or sets it to -5
+print("End value:")								--asking for endvalue
+local endvalue = tonumber(io.read()) or 5			--gets input, converts it to a number or sets it to 5
+print("Step:")									--asking for a step value
+local step = tonumber(io.read()) or 1				--gets input, converts it to a number or sets it to 1
+for x = startvalue, endvalue, step do				-- numeric for loop
+ print (fx , x)									-- printing fx and x	
 end
