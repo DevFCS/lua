4f16b16580ce27cfa295717a76b521501cefd7dc
trfunk <tristan.funk@ymail.com>
2013-10-17 18:00:32 +0200

    added description for [138]repulsionforce.lua

--- dailyprogrammer (reddit)/[138]repulsionforce.lua
+++ dailyprogrammer (reddit)/[138]repulsionforce.lua
@@ -1,3 +1,16 @@
+--[[ [138]repulsionforce.lua
+!!DOESNT WORK, WILL BE REWORKED!!
+This program computes the repulsion force for two electrons in 2D space. It assumes that the two particles have the same mass and charge. Note that Colomb's Law uses a constant, but I choose to omit that for the simplicity.
+
+Sample Input (takes input from "input.txt"):
+1 -5.2 3.8
+1 8.7 -4.1
+
+Sample Output:
+0.0039
+
+Taken from http://www.reddit.com/r/dailyprogrammer/comments/1ml669/091713_challenge_138_easy_repulsionforce/
+--]]
 local f = io.open("input.txt", "r")
 for line in io.lines("input.txt") do
   local mass1, xpos1, ypos1, mass2, xpos2, ypos2 = f.read("*number","*number","*number","*number","*number","*number")
