0d6623d0d94758279120c8e9502fc58fa951d43c
trfunk <tristan.funk@ymail.com>
2014-01-27 21:56:35 +0100

    worked on challenge 7

---
+++ dailyprogrammer (reddit)/[7]morsecode.lua
@@ -0,0 +1,35 @@
+--[[ Easy Challenge #7
+Write a program that can translate Morse code in the format of ...---...
+A space and a slash will be placed between words. ..- / --.-
+For bonus, add the capability of going from a string to Morse code.
+Super-bonus if your program can flash or beep the Morse.
+This is your Morse to translate:
+.... . .-.. .-.. --- / -.. .- .. .-.. -.-- / .--. .-. --- --. .-. .- -- -- . .-. / --. --- --- -.. / .-.. ..- -.-. -.- / --- -. / - .... . / -.-. .... .- .-.. .-.. . -. --. . ... / - --- -.. .- -.--
+--]]
+local morsealphabet = { ["a"] = ".-", ["b"] = "-...", ["c"] = "-.-.",    ["d"] = "-..", ["e"] = ".", ["f"] = "..-.", ["g"] = "--.", ["h"] = "....", ["i"] = "..", ["j"] = ".---", ["k"] = "-.-", ["l"] = ".-..", ["m"] = "--", ["n"] = "-.", ["o"] = "---", ["p"] = ".--.", ["q"] = "--.-", ["r"] = ".-.", ["s"] = "...", ["t"] = "-", ["u"] = "..-", ["v"] = "...-", ["w"] = ".--", ["x"] = "-..-", ["y"] = "-.--", ["z"] = "--..", [" "] = "/" }
+local reversemorsealphabet = {}
+for k,v in pairs(morsealphabet) do reversemorsealphabet[v] = k end
+local morsedtable = {}
+local function morse(x)
+     local morsed =  string.gsub(string.lower(x), "%Z", morsealphabet)
+     local morsed = string.gsub(morsed, "/", " / ")
+     return morsed
+end
+local function demorse(x)
+     local demorsed = string.gsub(x, "%S+", reversemorsealphabet)
+     return demorsed
+end
+
+print("Do you want to (m)orse or (d)emorse?")
+local input = io.read()
+if string.lower(input) == "m" then do
+      print("What do you want to morse:")
+      input2 = io.read()
+      print(morse(input2))
+elseif string.lower(input) == "d" then do
+      print("What do you want to demorse:")
+      input2 = io.read()
+      print(demorse(input2))
+else 
+      print("Invalid character. Please enter "m" to morse or "d" to demorse")
+end
